f9bbcdfa07ed1998345623a72fb693de4416a4b2
PatriciaTakagi <ptakagiayumi2@gmail.com>
2026-06-14 18:21:15 -0300

    ultima atualização do dia 14jun26

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
+//Código JavaScript que implementa a function para enviar formulário
 function enviarFormulario(){
+    //retorna os valores digitados nos campos do formulário
     let nome=document.getElementById("nome").value;
     let email=document.getElementById("email").value;
     let mensagem=document.getElementById("mensagem").value;
 
-    //validação dos campos
+    //validação dos campos obrigatórios (nome, e-mail, mensagem)
     if(nome===""||email===""||mensagem===""){
+        //retorna mensagem de alerta se um dos campos estiver vazio
         alert("Preencha todos os campos!");
-        return
+        return //interrompe a execução da função caso campos obrigatórios esteja vazios
     }
-   //validação do e-mail
-   //if(!document.getElementById("email").checkValidity())
-   
-   if (!email.includes("@")||!email.includes(".")){
-    alert("E-mail inválido");
+
+   //valida se o formato do e-mail é valido (se possui "@" e ".")
+    if (!email.includes("@")||!email.includes(".")){
+        //retorna mensagem de alerta se o formato do e-mail informado não for válido
+    alert("E-mail inválido");//interrompe a execução da função caso formato do e-mail inválido
     return;
    } 
-   //simulação de envio
+
+   //simulação de envio (dados armazenados no Local Storage do navegador)
    localStorage.setItem("nome",nome);
    localStorage.setItem("email",email);
    localStorage.setItem("mensagem",mensagem);
-
+   //menagem de confirmação de envio para o usuário
    alert("Mensagem enviada com sucesso");
 
-   //limpa os campos
+   //limpa os campos do formulário após o envio
    document.getElementById("nome").value="";
    document.getElementById("email").value="";
    document.getElementById("mensagem").value="";
